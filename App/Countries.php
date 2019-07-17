92c7ff9861372843e38caaff0247becba883ca1a
Le_Futuriste <spamfree@matthieubessat.fr>
2019-07-17 17:10:52 +0200

    feat: add Countries helper

---
+++ App/Countries.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+class Countries
+{
+    public function getCountriesDirectory()
+    {
+        return dirname(__DIR__) . '/public/locales';
+    }
+
+    /**
+     * Get the countries codes with their associated name in a given locale
+     * Return null if a the locale code is invalid
+     *
+     * @param string $locale
+     * @return array|null
+     */
+    public function getLocalizedCountries(string $locale)
+    {
+        $path = $this->getCountriesDirectory() . '/' . $locale . '.json';
+        if (!file_exists($path)) {
+            return null;
+        }
+        $json = json_decode(file_get_contents($path), true);
+        return $json;
+    }
+
+    /**
+     * Known if whether or not the input ISO 3166-1 country code is valid
+     *
+     * @param string $code
+     * @return bool
+     */
+    public function isCountryCodeValid(string $code): bool
+    {
+        $path = $this->getCountriesDirectory() . '/en.json';
+        $json = json_decode(file_get_contents($path), true);
+        $countries = array_filter($json['countries'], function ($country) use ($code) {
+            return $country === $code;
+        });
+        return isset($countries[0]) && $countries[0]['code'] === $code;
+    }
+}

f1b92ef05dd0402f78c18bd9ae130d818a54c371
Matthieu Bessat <spamfree@matthieubessat.fr>
2019-10-24 23:40:32 +0200

    feat(Console): add is_development column to console table

---
+++ App/Database/Migrations/20191024180610_add_is_development_column_to_consoles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class AddIsDevelopmentColumnToConsolesTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $this->table('consoles')
+            ->addColumn('is_development', 'boolean', ['default' => false, 'null' => true])
+            ->update();
+    }
+}

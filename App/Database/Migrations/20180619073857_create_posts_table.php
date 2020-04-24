e003dfd544a7f6ba6e4721a543ae9dd3a7fb4122
Matthieu Bessat <spamfree@matthieubessat.fr>
2020-04-24 13:23:47 +0200

    refactor(Migrations): remove useless comments on change() methods inside migration classes

--- App/Database/Migrations/20180619073857_create_posts_table.php
+++ App/Database/Migrations/20180619073857_create_posts_table.php
@@ -5,40 +5,19 @@ use Phinx\Migration\AbstractMigration;
 
 class CreatePostsTable extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
     public function change()
     {
-		$this->table('posts')
-			->addColumn('title', 'string')
-			->addColumn('slug', 'string')
-			->addColumn('description', 'text')
-			->addColumn('content', 'text')
-			->addColumn("created_at", 'datetime', ['null' => true])
-			->addColumn("updated_at", 'datetime', ['null' => true])
-			->create();
+        $this->table('posts')
+            ->addColumn('title', 'string')
+            ->addColumn('slug', 'string')
+            ->addColumn('description', 'text')
+            ->addColumn('content', 'text')
+            ->addColumn("created_at", 'datetime', ['null' => true])
+            ->addColumn("updated_at", 'datetime', ['null' => true])
+            ->create();
 
-		$this->table('posts')
-			->changeColumn('id', 'string')
-			->update();
+        $this->table('posts')
+            ->changeColumn('id', 'string')
+            ->update();
     }
 }

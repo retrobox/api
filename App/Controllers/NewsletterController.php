71611e52c93050c10c747d9ab899b8e8717dfab9
Matthieu Bessat <spamfree@matthieubessat.fr>
2020-05-14 12:10:46 +0200

    feat(Newsletter): ignore events from mailchimp for email containing keywords 'events' AND 'ignore'
    nb: this is to avoid sending useless webhook to discord when doing api integration test. But there is no way to tell mailchimp to send the events to a specific endpoint so we are doing that it's better than noting.

--- App/Controllers/NewsletterController.php
+++ App/Controllers/NewsletterController.php
@@ -51,15 +51,24 @@ class NewsletterController extends Controller
 
     public function postEvent(ServerRequestInterface $request, Response $response)
     {
-        $data = $request->getParsedBody();
-        $data = json_decode(json_encode($data), true);
+        $data = json_decode(json_encode($request->getParsedBody()), true);
         $discordWebHook = new Client($this->container->get('mailchimp')['discord_webhook']);
         $embed = new Embed();
-        switch ($data['type']) {
-            case "subscribe":
+        $notes = [];
+        if (!isset($data['type']) || empty($data['type'])) {
+            return $response->withJson([
+                'success' => false,
+                'errors' => [['message' => 'Missing type field']]
+            ], 400);
+        }
+        if ($data['type'] === 'subscribe') {
+            $address = $data['data']['email'];
+            if (strpos($address, 'ignore') !== false && strpos($address, 'event') !== false) {
+                $notes[] = 'This event was ignored because of the email address format';
+            } else {
                 $embed->color("27ae60");
                 $embed->title("New subscriber!");
-                $embed->field("Email", $data['data']['email']);
+                $embed->field("Email", $address);
                 $embed->field("Ip", $data['data']['ip_opt']);
                 $embed->field("At", $data['fired_at']);
                 $embed->field("Id", $data['data']['id']);
@@ -71,10 +80,13 @@ class NewsletterController extends Controller
                         'errors' => [['message' => 'Error with the discord webhook api']]
                     ], 400);
                 }
-                break;
+            }
+        } else {
+            $notes[] = 'This event was ignored because is it not in the server scope';
         }
         return $response->withJson([
-            'success' => true
+            'success' => true,
+            'notes' => $notes
         ]);
     }
 }

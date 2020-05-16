621f0427c41316aa096a279855656e9805d7584f
Matthieu Bessat <spamfree@matthieubessat.fr>
2020-05-16 15:00:51 +0200

    fix: add permission check on update and destroy user GraphQL mutation
    nb: this might be a critical security fix

--- App/GraphQL/Query/User.php
+++ App/GraphQL/Query/User.php
@@ -4,6 +4,7 @@ namespace App\GraphQL\Query;
 
 use App\Auth\Session;
 use App\GraphQL\Types;
+use App\Utils\Countries;
 use Exception;
 use GraphQL\Type\Definition\InputObjectType;
 use GraphQL\Type\Definition\Type;
@@ -16,26 +17,22 @@ class User
         return [
             'type' => Type::listOf(Types::user()),
             'description' => 'Get many users',
-            'args' => [
-                [
-                    'name' => 'limit',
-                    'description' => 'Number of items to get',
-                    'type' => Type::int(),
-                    'defaultValue' => -1
-                ],
-                [
-                    'name' => 'orderBy',
-                    'description' => 'Order by a field',
-                    'type' => Type::string(),
-                    'defaultValue' => 'created_at'
-                ],
-                [
-                    'name' => 'orderDir',
-                    'description' => 'Direction of the order',
-                    'type' => Type::string(),
-                    'defaultValue' => 'desc'
-                ]
-            ],
+            'args' => [[
+                'name' => 'limit',
+                'description' => 'Number of items to get',
+                'type' => Type::int(),
+                'defaultValue' => -1
+            ], [
+                'name' => 'orderBy',
+                'description' => 'Order by a field',
+                'type' => Type::string(),
+                'defaultValue' => 'created_at'
+            ], [
+                'name' => 'orderDir',
+                'description' => 'Direction of the order',
+                'type' => Type::string(),
+                'defaultValue' => 'desc'
+            ]],
             'resolve' => function (ContainerInterface $container, $args) {
                 //admin only
                 if ($container->get(Session::class)->isAdmin()) {
@@ -55,14 +52,12 @@ class User
         return [
             'type' => Types::user(),
             'description' => 'Get a user by id',
-            'args' => [
-                [
-                    'name' => 'id',
-                    'description' => 'The STAIL.EU uuid of the user account',
-                    'type' => Type::string(),
-                    'defaultValue' => NULL
-                ]
-            ],
+            'args' => [[
+                'name' => 'id',
+                'description' => 'The STAIL.EU uuid of the user account',
+                'type' => Type::string(),
+                'defaultValue' => NULL
+            ]],
             'resolve' => function (ContainerInterface $container, $args) {
                 $userId = $container->get(Session::class)->getUserId();
                 if ($args['id'] === NULL) {
@@ -86,53 +81,53 @@ class User
     {
         return [
             'type' => Type::boolean(),
-            'args' => [
-                [
-                    'name' => 'user',
-                    'description' => 'User to update',
-                    'type' => new InputObjectType([
-                        'name' => 'UserUpdateInput',
-                        'fields' => [
-                            'id' => ['type' => Type::nonNull(Type::string())],
-                            'is_admin' => ['type' => Type::boolean()],
-                            'first_name' => ['type' => Type::string()],
-                            'last_name' => ['type' => Type::string()],
-                            'address_first_line' => ['type' => Type::string()],
-                            'address_second_line' => ['type' => Type::string()],
-                            'address_postal_code' => ['type' => Type::string()],
-                            'address_city' => ['type' => Type::string()],
-                            'address_country' => ['type' => Type::string()]
-                        ]
-                    ])
-                ]
-            ],
-            'resolve' => function ($_, $args) {
+            'args' => [[
+                'name' => 'user',
+                'description' => 'User to update',
+                'type' => new InputObjectType([
+                    'name' => 'UserUpdateInput',
+                    'fields' => [
+                        'id' => ['type' => Type::nonNull(Type::string())],
+                        'is_admin' => ['type' => Type::boolean()],
+                        'first_name' => ['type' => Type::string()],
+                        'last_name' => ['type' => Type::string()],
+                        'address_first_line' => ['type' => Type::string()],
+                        'address_second_line' => ['type' => Type::string()],
+                        'address_postal_code' => ['type' => Type::string()],
+                        'address_city' => ['type' => Type::string()],
+                        'address_country' => ['type' => Type::string()]
+                    ]
+                ])
+            ]],
+            'resolve' => function (ContainerInterface $container, $args) {
+                $session = $container->get(Session::class);
+                /* @var $user \App\Models\User */
                 $user = \App\Models\User::query()->find($args['user']['id']);
+                if (!$session->isAdmin() && $session->getUserId() !== $args['user']['id']) {
+                    return new Exception('Forbidden', 403);
+                }
                 if ($user == NULL) {
                     return new Exception('Unknown user', 404);
                 }
-                if (isset($args['user']['first_name'])) {
-                    $user['first_name'] = $args['user']['first_name'];
-                }
-                if (isset($args['user']['last_name'])) {
-                    $user['last_name'] = $args['user']['last_name'];
-                }
-                if (isset($args['user']['address_first_line'])) {
-                    $user['address_first_line'] = $args['user']['address_first_line'];
-                }
-                if (isset($args['user']['address_second_line'])) {
-                    $user['address_second_line'] = $args['user']['address_second_line'];
-                }
-                if (isset($args['user']['address_postal_code'])) {
-                    $user['address_postal_code'] = $args['user']['address_postal_code'];
-                }
-                if (isset($args['user']['address_city'])) {
-                    $user['address_city'] = $args['user']['address_city'];
-                }
-                if (isset($args['user']['address_country'])) {
-                    $user['address_country'] = $args['user']['address_country'];
-                }
-                if (isset($args['user']['is_admin'])) {
+                $countriesHelper = $container->get(Countries::class);
+                if (
+                    isset($args['user']['address_country']) &&
+                    !$countriesHelper->isCountryCodeValid($args['user']['address_country'])
+                )
+                    return new Exception('Invalid country code', 400);
+                $user->setAttributesFromGraphQL($args['user'], [
+                    'first_name',
+                    'last_name',
+                    'address_first_line',
+                    'address_second_line',
+                    'address_postal_code',
+                    'address_city',
+                    'address_country'
+                ]);
+                if (isset($args['user']['is_admin']) && is_bool($args['user']['is_admin'])) {
+                    if (!$session->isAdmin()) {
+                        return new Exception('Forbidden, you must be a admin to change user admin level', 403);
+                    }
                     $user['is_admin'] = $args['user']['is_admin'];
                 }
                 return $user->save();
@@ -140,18 +135,16 @@ class User
         ];
     }
 
-    public static function destroy() {
+    public static function destroy()
+    {
         return [
             'type' => Type::boolean(),
-            'args' => [
-                'id' => [
-                    'type' => Type::id()
-                ]
-            ],
+            'args' => ['id' => ['type' => Type::id()]],
             'resolve' => function (ContainerInterface $container, $args) {
-                if ($container->get(Session::class)->isAdmin()) {
+                $session = $container->get(Session::class);
+                if ($session->isAdmin() || $session->getUserId() === $args['id']) {
                     $user = \App\Models\User::query()->find($args['id']);
-                    if ($user == NULL){
+                    if ($user == NULL) {
                         return new Exception('Unknown User', 404);
                     }
                     return \App\Models\User::destroy($user['id']);
--- App/Models/Model.php
+++ App/Models/Model.php
@@ -7,9 +7,11 @@ class Model extends \Illuminate\Database\Eloquent\Model
     public function setAttributesFromGraphQL($graphQLArgs, array $fields): self
     {
         foreach ($fields as $field){
-            if (isset($graphQLArgs[$field]) && (!empty($graphQLArgs[$field]) || is_bool($graphQLArgs[$field]))){
+            if (
+                isset($graphQLArgs[$field]) &&
+                (!empty($graphQLArgs[$field]) || is_bool($graphQLArgs[$field]))
+            )
                 $this[$field] = $graphQLArgs[$field];
-            }
         }
         return $this;
     }
